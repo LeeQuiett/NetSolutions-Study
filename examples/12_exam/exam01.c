3680dbf32cc8355979a4f2c01627096b0a1bb057
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-09 01:11:02 +0900

    세 정수의 누적 및 출력

---
+++ examples/12_exam/exam01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int nInput = 0, nTotal = 0;
+
+	scanf_s("%d", &nInput);
+	nTotal += nInput;
+	scanf_s("%d", &nInput);
+	nTotal += nInput;
+	scanf_s("%d%*c", &nInput);
+	nTotal += nInput;
+
+	printf("Total: %d", nTotal);
+	return 0;
+}
