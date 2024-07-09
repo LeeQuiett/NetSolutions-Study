5156c9ce5dd336594fbb16d6147494d6454a7679
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-09 11:35:16 +0900

    논리합, 논리곱

---
+++ examples/15_opLogical/opLogical01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int nInput = 0, bResult = 0;
+
+	scanf_s("%d", &nInput);
+	bResult = nInput < 4 || nInput >= 60;
+	printf("Result: %d (1: True, 0: Flase)", bResult);
+
+	return 0;
+}
