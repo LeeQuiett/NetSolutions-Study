4cc54a35bc9208eeeca961c084f59b1a3ac0ef26
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-11 01:30:05 +0900

    문자열의 길이 측정, 널 문자

--- examples/30_arrayString/arrString01.c
+++ examples/30_arrayString/arrString01.c
@@ -2,7 +2,7 @@
 
 int main(void) {
 	int list[5] = { 30,40,10,50,20 };
-	char buffer[6] = { 'H', 'e', 'l', 'l', 'o','\0' };
+	char buffer[6] = { 'H', 'e', 'l', 'l', 'o', '\0'};
 
 	char data[8] = { "Hello" };
 
