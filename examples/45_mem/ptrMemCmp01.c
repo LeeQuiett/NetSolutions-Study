37eac4e3957fc05f3ee1d7df1f762b7bae37d36f
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-14 23:47:11 +0900

    문자열 길이 논리 오류 수정

--- examples/45_mem/ptrMemCmp01.c
+++ examples/45_mem/ptrMemCmp01.c
@@ -2,16 +2,18 @@
 #include <string.h>
 
 int main(void) {
-	char szBuffer[12] = { "TestString" };
-	char* pszData = "TestString";
+    char szBuffer[12] = { "TestString" };
+    char* pszData = "TestString";
+    size_t length = strlen(pszData);  // 문자열의 실제 길이를 구합니다.
 
-	//두 메모리에 저장된 값이 같은 경우
-	printf("%d\n", memcmp(szBuffer, pszData, sizeof(szBuffer)));
+    // 두 메모리에 저장된 값이 같은 경우
+    printf("%d\n", memcmp(szBuffer, pszData, length));
 
-	//왼쪽("teststring")이 더 큰 경우
-	printf("%d\n", memcmp("testString", pszData, sizeof(szBuffer)));
+    // 왼쪽("teststring")이 더 큰 경우
+    printf("%d\n", memcmp("testString", pszData, length));
 
-	//오른쪽(pszData)이 더 큰 경우
-	printf("%d\n", memcmp("DataString", pszData, sizeof(szBuffer)));
-	return 0;
+    // 오른쪽(pszData)이 더 큰 경우
+    printf("%d\n", memcmp("DataString", pszData, length));
+
+    return 0;
 }
