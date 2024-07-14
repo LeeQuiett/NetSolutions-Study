35cf8e536ad7d37bd6a7f02722017ad6b3a0d3e8
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-14 22:35:50 +0900

    동적 할당된 메모리 크기 확인

--- examples/43_memory/ptrMemInit02.c
+++ examples/43_memory/ptrMemInit02.c
@@ -4,10 +4,10 @@
 int main(void) {
 	//선언한 배열 요소의 크기를 기술하지 않았지만 초깃값을 기준으로
 	//자동으로 크기가 결정 된다(NULL 문자 저장될 크기 포함).
-	char szBuffer[] = { "Hello" };
+	char szBuffer[] = { "Hello" }; //지역변수인 szBuffer 배열이 사용하는 스택 메모리의 초깃값이 됨
 
 	//"Hello" 문자열이 저장된 메모리의 주소로 초기화되는 포인터 변수
-	char* pszBuffer = "Hello";
+	char* pszBuffer = "Hello"; //데이터 영역 중 읽기전용 영역 어딘가에 "Hello"라는 문자열이 저장된 것
 
 	//동적 할당한 메모리의 주소가 저장될 포인터 선언 및 정의
 	char* pszData = NULL;
