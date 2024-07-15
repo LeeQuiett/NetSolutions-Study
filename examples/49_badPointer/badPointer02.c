1e19803b62638bffd118efefbeec22de56a8537b
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-15 15:51:52 +0900

    경계를 넘어 입출력 했으나 오류 안남

---
+++ examples/49_badPointer/badPointer02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	char* pszBuffer = NULL;
+
+	pszBuffer = (char*)malloc(12);
+	gets(pszBuffer);
+	puts(pszBuffer);
+
+	//오류 확인을 위해 의도적으로 해제하지 않음
+	//free(pszBuffer);
+	return 0;
+}
