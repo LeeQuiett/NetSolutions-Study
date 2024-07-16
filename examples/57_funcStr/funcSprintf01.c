e7db81423ef204d10a80be5957ba445f0f1d5949
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-16 15:01:34 +0900

    sprintf() 함수 사용하여 문자열 이어붙임

---
+++ examples/57_funcStr/funcSprintf01.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdio.h>
+
+int main(void) {
+	char szPath[128] = { 0 };
+	char szBuffer[128] = { 0 };
+
+	printf("Input path: ");
+	gets(szBuffer);
+
+	sprintf(szPath, "C:\\Program Files\\%s", szBuffer);
+	puts(szPath);
+	return 0;
+}
